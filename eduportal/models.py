5ec0c3f94d8aa73e31116123426181013840bb70
K-T-P <koroush.t81@gmail.com>
2024-04-14 16:31:12 +0330

    feat: Add student models Student model added. There are no fields yet.

--- eduportal/models.py
+++ eduportal/models.py
@@ -1,3 +1,10 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user_profile"
+    )
