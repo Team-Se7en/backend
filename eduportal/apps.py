363e15319a4c17990b550002937b01df8b713476
p3dr4m0098 <pedramkhojasterad@gmail.com>
2024-04-16 01:57:59 +0330

    feat: Add signal for profile creation at signup

--- eduportal/apps.py
+++ eduportal/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class EduportalConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'eduportal'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "eduportal"
+
+    def ready(self) -> None:
+        import eduportal.signals.handlers 
