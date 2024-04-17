84b8fcd26b0de4b04b353dc133dc18706680087f
K-T-P <koroush.t81@gmail.com>
2024-04-17 15:03:25 +0330

    feat: add student profile api some fields added to student model just to test endpoints.

--- eduportal/views.py
+++ eduportal/views.py
@@ -1,11 +1,40 @@
+from django.shortcuts import get_object_or_404
 from .models import Student
-from .serializers import StudentGetListSerializer
+from . import serializers
 from rest_framework import viewsets
 from rest_framework import mixins
+from rest_framework.permissions import IsAuthenticated
+from djoser.permissions import CurrentUserOrAdmin
+from pprint import pprint
+from rest_framework.response import Response
+from rest_framework import status
 
 class StudentGetListViewSet(
     mixins.ListModelMixin,
     viewsets.GenericViewSet,
 ):
     queryset = Student.objects.all()
-    serializer_class = StudentGetListSerializer
+    serializer_class = serializers.StudentGetListSerializer
+
+
+class StudentProfileViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Student.objects.all()
+    serializer_class = serializers.StudentProfileSerializer
+    permission_classes = [IsAuthenticated, CurrentUserOrAdmin]
+    http_method_names = [
+        "get",
+        "patch",
+        "delete",
+    ]
+    def destroy(self, request, *args, **kwargs):
+        student_id = self.kwargs["pk"]
+        student = get_object_or_404(Student, pk=student_id)
+        user = student.user 
+        student.delete()
+        user.delete()
+        return Response(status=status.HTTP_202_ACCEPTED)
