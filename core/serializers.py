f33bd237382bbdb7885a32aab309ae3623a225ac
p3dr4m0098 <pedramkhojasterad@gmail.com>
2024-04-22 20:45:41 +0330

    feat: Add is_student field to login response

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,24 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import update_last_login
 from djoser.serializers import UserCreateSerializer as BaseUCS
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.settings import api_settings
+
+class TokenObtainSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        refresh = self.get_token(self.user)
+
+        data["refresh"] = str(refresh)
+        data["access"] = str(refresh.access_token)
+        data["is_student"] = self.user.is_student
+
+        if api_settings.UPDATE_LAST_LOGIN:
+            update_last_login(None, self.user)
+
+        return data
+
 
 User = get_user_model()
 
--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from rest_framework.routers import SimpleRouter
 from . import views
 
 urlpatterns = [
+    path("jwt/create/", views.CutomObtainPairView.as_view(), name="customtoken"),
     path("", include("djoser.urls")),
     path("", include("djoser.urls.jwt")),
 ]
