69872a3570f176fbe50b21c5a9e38c99a0972fa4
p3dr4m0098 <pedramkhojasterad@gmail.com>
2024-04-24 21:13:16 +0330

    fix: Add custom error messages for login.

--- core/serializers.py
+++ core/serializers.py
@@ -1,12 +1,30 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import update_last_login
 from djoser.serializers import UserCreateSerializer as BaseUCS
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.settings import api_settings
 
+
 class TokenObtainSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
+
+        user = User.objects.filter(email=attrs["email"])
+        if not user.exists():
+            raise exceptions.AuthenticationFailed(
+                "No account found with this email. Create an account now."
+            )
+
+        user = user.first()
+
+        if not user.is_active:
+            raise exceptions.AuthenticationFailed(
+                "Your account has not been activated."
+            )
+
+        if not user.check_password(attrs["password"]):
+            raise exceptions.AuthenticationFailed("Your password is incorrect.")
+
         data = super().validate(attrs)
         refresh = self.get_token(self.user)
 
