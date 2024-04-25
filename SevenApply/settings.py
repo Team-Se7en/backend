4c3f9e0c9fa6988855602d39d2fde72b89fac559
p3dr4m0098 <pedramkhojasterad@gmail.com>
2024-04-25 21:24:11 +0330

    feat: Add additional SMTP servers

--- SevenApply/settings.py
+++ SevenApply/settings.py
@@ -99,12 +99,7 @@ DATABASES = {
     )
 }
 
-EMAIL_BACKEND = env("EMAIL_BACKEND")
-EMAIL_HOST = env("EMAIL_HOST")
-EMAIL_PORT = env("EMAIL_PORT")
-EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = env("EMAIL_USE_TLS")
+EMAIL_BACKEND = "SevenApply.smtp.FailoverSMTPBackend"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
