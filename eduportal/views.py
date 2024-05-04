b2f94f947758b5de10f10bc8d39256f5d3a4a0f4
K-T-P <koroush.t81@gmail.com>
2024-05-04 20:14:40 +0330

    revert: comment code which will not be used yet.

--- eduportal/views.py
+++ eduportal/views.py
@@ -389,11 +389,11 @@ class ProfessorOwnPositionFilteringViewSet(ListModelMixin, GenericViewSet):
         base_query = Position.objects.select_related("professor").filter(
             professor__id=self.request.user.professor.id
         )
-        if self.request.query_params.get('ordering'):
-            if 'position_start_date' in self.request.query_params['ordering']:
-                base_query = base_query.filter(
-                position_start_date__gte=timezone.now().date()
-                )
+        #if self.request.query_params.get('ordering'):
+        #    if 'position_start_date' in self.request.query_params['ordering']:
+        #        base_query = base_query.filter(
+        #        position_start_date__gte=timezone.now().date()
+        #        )
         filter_options = self.request.query_params
         min_fee = filter_options.get("min_fee")
         max_fee = filter_options.get("max_fee")
@@ -433,11 +433,11 @@ class ProfessorOtherPositionFilteringViewSet(ListModelMixin, GenericViewSet):
         base_query = Position.objects.select_related("professor").exclude(
             professor__id=self.request.user.professor.id
         )
-        if self.request.query_params.get('ordering'):
-            if 'position_start_date' in self.request.query_params['ordering']:
-                base_query = base_query.filter(
-                position_start_date__gte=timezone.now().date()
-                )
+        #if self.request.query_params.get('ordering'):
+        #    if 'position_start_date' in self.request.query_params['ordering']:
+        #        base_query = base_query.filter(
+        #        position_start_date__gte=timezone.now().date()
+        #        )
         filter_options = self.request.query_params
         min_fee = filter_options.get("min_fee")
         max_fee = filter_options.get("max_fee")
