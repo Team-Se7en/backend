f9c626961f46b4e7175ad6347c4bd68e9fc63ddb
p3dr4m0098 <pedramkhojasterad@gmail.com>
2024-05-07 00:47:54 +0330

    feat: Add new queries to university view

--- eduportal/serializers.py
+++ eduportal/serializers.py
@@ -80,10 +80,22 @@ class UserDetailSerializer(
 
 class UniversitySerializer(serializers.ModelSerializer):
     country = CountrySerializer(name_only=True)
+    position_count = serializers.IntegerField()
+    recent_positions = serializers.SerializerMethodField()
 
     class Meta:
         model = University
         fields = "__all__"
+
+    def get_recent_positions(self, obj):
+        poses = (
+            Position.objects.filter(professor__university=obj)
+            .select_related("professor", "professor__user", "professor__university")
+            .prefetch_related("tags")
+        )
+
+        poses = poses.filter(start_date__lte=timezone.now()).order_by("-start_date")[:5]
+        return BasePositionListSerializer(poses, many=True).data
 
 
 # Student Serializers ----------------------------------------------------------
