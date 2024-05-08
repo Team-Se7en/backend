bcf4926ac2f0708cdf0175feffb1bffe248e69f5
p3dr4m0098 <pedramkhojasterad@gmail.com>
2024-05-09 02:39:31 +0330

    feat: Add utility function for filling CV's

--- eduportal/utils/models.py
+++
@@ -1,8 +0,0 @@
-from eduportal.models import SoftSkill
-from .field_choices import SoftSkillChoices
-
-def update_soft_skills():
-    SoftSkill.objects.all().delete()
-
-    for skill in SoftSkillChoices:
-        SoftSkill.objects.create(skill=skill)
