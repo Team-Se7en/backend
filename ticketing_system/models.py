ff28501d7048a27ff2ad39c4b558573b44f23577
K-T-P <koroush.t81@gmail.com>
2024-05-26 16:21:03 +0330

    feat: Add basic chat system model.

--- ticketing_system/models.py
+++ ticketing_system/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+USER_MODEL = get_user_model()
+
+
+class ChatSystem(models.Model):
+    group_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Message(models.Model):
+    text = models.TextField(max_length=255)
+    send_time = models.DateTimeField(auto_now_add=True)
+    related_chat_group = models.ForeignKey(
+        ChatSystem,
+        on_delete=models.CASCADE,
+    )
+
+
+class ChatMembers(models.Model):
+    chat = models.OneToOneField(
+        ChatSystem,
+        on_delete=models.CASCADE,
+        related_name="chat",
+    )
+    participants = models.ManyToManyField(
+        USER_MODEL,
+    )
