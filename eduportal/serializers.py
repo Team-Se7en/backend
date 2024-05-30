eff124a08e81563e1d7486cba2ea0299cb1c4ade
p3dr4m0098 <pedramkhojasterad@gmail.com>
2024-05-30 21:07:54 +0330

    fix: Make OwnerPositionDetail status field display the long form

--- eduportal/serializers.py
+++ eduportal/serializers.py
@@ -434,6 +434,7 @@ class OwnerPositionRequestDetailSerializer(
     serializers.ModelSerializer,
 ):
     student = SimpleStudentSerializer()
+    status = serializers.CharField(source="get_status_display")
 
     class Meta:
         model = Request
