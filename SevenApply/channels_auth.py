4f8ecf4d13878a41309bdb3aa0c112fe88691335
p3dr4m0098 <pedramkhojasterad@gmail.com>
2024-06-10 21:03:04 +0330

    feat: Add query parameter authorization to WS connections

--- SevenApply/channels_auth.py
+++ SevenApply/channels_auth.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AnonymousUser
 from jwt import decode as jwt_decode
+from urllib.parse import parse_qsl
 
 User = get_user_model()
 
@@ -24,16 +25,31 @@ class JwtAuthMiddleware(BaseMiddleware):
         self.inner = inner
 
     async def __call__(self, scope, receive, send):
-        headers = dict(scope["headers"])
         scope["user"] = AnonymousUser()
-        if b"authorization" in headers:
+        token_key = None
+
+        if "query_string" in scope:
+            query_string = scope["query_string"].decode()
+            query_params = dict(parse_qsl(query_string))
+            token_key = query_params.get("token", None)
+
+        if not token_key:
+            headers = dict(scope["headers"])
+            if b"authorization" in headers:
+                try:
+                    token_name, token_key = headers[b"authorization"].decode().split()
+                    if token_name != "JWT":
+                        token_key = None
+                except:
+                    pass
+
+        if token_key:
+            print(f"token key:\t{token_key}")
             try:
-                token_name, token_key = headers[b"authorization"].decode().split()
-                if token_name == "JWT":
-                    decoded_data = jwt_decode(
-                        token_key, settings.SECRET_KEY, algorithms=["HS256"]
-                    )
-                    scope["user"] = await get_user(validated_token=decoded_data)
+                decoded_data = jwt_decode(
+                    token_key, settings.SECRET_KEY, algorithms=["HS256"]
+                )
+                scope["user"] = await get_user(validated_token=decoded_data)
             except:
                 pass
         return await super().__call__(scope, receive, send)
