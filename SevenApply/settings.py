85a72b67dcd9c824f15fc744fa36285e9aac9bc5
p3dr4m0098 <pedramkhojasterad@gmail.com>
2024-07-03 21:55:39 +0330

    "config: Change login token expiry time to 7 days"

--- SevenApply/settings.py
+++ SevenApply/settings.py
@@ -194,8 +194,8 @@ DJOSER = {
 
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("JWT",),
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
     "UPDATE_LAST_LOGIN": True,
 }
 
