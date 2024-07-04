e252868a684cd5853c3aefdf23d260cf8c1ff4f6
p3dr4m0098 <pedramkhojasterad@gmail.com>
2024-07-05 00:15:26 +0330

    chore: Remove notification debugging log prints

--- eduportal/serializers.py
+++ eduportal/serializers.py
@@ -736,8 +736,7 @@ class NotificationSerializer(serializers.ModelSerializer):
         stu = None
 
         request = self.get_model_item(obj, Request)
-        print(request)
-        print(vars(request))
+
         if request:
             stu = request.student
         else:
@@ -748,10 +747,8 @@ class NotificationSerializer(serializers.ModelSerializer):
         return None
 
     def get_model_item(self, obj: Notification, model):
-        print(vars(obj))
         item = None
         item_list = getattr(obj, f"{model._meta.app_label}_{model._meta.model_name}_items", [])
-        print(item_list)
         if item_list:
             item = item_list[0]
         if item:
